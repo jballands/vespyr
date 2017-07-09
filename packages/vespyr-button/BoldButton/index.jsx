ad606e668bc0f059966148fa1bedbc8ab7a981e2
Jon Ballands <jballands@gmail.com>
2017-07-09 14:47:24 -0500

    Working on getting babel set up

---
+++ packages/vespyr-button/BoldButton/index.jsx
@@ -0,0 +1,18 @@
+//
+//	jballands/vespyr-button
+//	BoldButton/index.js
+//	
+//	© 2017 Jonathan Ballands
+//
+
+import React from 'react';
+
+export default class BoldButton extends React.Component {
+
+	render () {
+		return (
+			<span>Hello world!</span>
+		);
+	}
+
+}
